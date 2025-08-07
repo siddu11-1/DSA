7e7dd53af9865ee993e7ba33c98944291fc4acd3
siddu11-1 <givvasiddhartha@gmail.com>
2025-08-07 11:31:12 +0530

    Time: 0 ms (100%), Space: 13 MB (56.96%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i]>=target:
+                return i
+            
+        return len(nums)    
+        
