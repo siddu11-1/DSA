6ecddb5187f56eea90c4aae186f19bc78e6b5f4e
siddu11-1 <givvasiddhartha@gmail.com>
2025-09-02 09:02:27 +0530

    Time: 0 ms (100%), Space: 12.6 MB (13.23%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        if not nums or len(nums) == 1:
+            return nums
+
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Reverse the suffix only if i + 1 < len(nums)
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+        return nums
+        
