7ffb36ec68c7ea7e24c51f49c6b27fd0c1917969
siddu11-1 <givvasiddhartha@gmail.com>
2025-10-03 15:56:15 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,16 +1,17 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        
-        p1 = m - 1  # Pointer for nums1's actual elements
-        p2 = n - 1  # Pointer for nums2
-        p = m + n - 1  # Pointer for placement in nums1
-
-        # Merge in reverse order
-        while p2 >= 0:
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        i=m-1
+        j=n-1
+        k=m+n-1
+        while i>=0 and j>=0:
+            if nums1[i]>=nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+        while j>=0:
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
