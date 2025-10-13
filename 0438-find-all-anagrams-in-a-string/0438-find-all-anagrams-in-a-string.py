345badd98319f2b1d11d35e1c8f245cc2d6ba036
siddu11-1 <givvasiddhartha@gmail.com>
2025-10-13 13:36:37 +0530

    Time: 42 ms (85.06%), Space: 13.6 MB (48.68%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        
+        if len(p) > len(s):
+            return []
+
+        result = []
+        p_count = [0] * 26
+        s_count = [0] * 26
+
+        for i in range(len(p)):
+            p_count[ord(p[i]) - ord('a')] += 1
+            s_count[ord(s[i]) - ord('a')] += 1
+
+        if s_count == p_count:
+            result.append(0)
+
+        for i in range(len(p), len(s)):
+            s_count[ord(s[i]) - ord('a')] += 1
+            s_count[ord(s[i - len(p)]) - ord('a')] -= 1
+            if s_count == p_count:
+                result.append(i - len(p) + 1)
+
+        return result
+        
