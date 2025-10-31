b00d440bb5fe806df277d3435ad4a723746c8348
siddu11-1 <givvasiddhartha@gmail.com>
2025-10-31 13:19:02 +0530

    Time: 43 ms (43.89%), Space: 12.8 MB (37.57%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,29 @@
+from collections import Counter
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        len1, len2 = len(s1), len(s2)
+        if len1 > len2:
+            return False
+
+        count1 = Counter(s1)
+        count2 = Counter(s2[:len1])
+
+        if count1 == count2:
+            return True
+
+        for i in range(len1, len2):
+            count2[s2[i]] += 1
+            count2[s2[i - len1]] -= 1
+
+            if count2[s2[i - len1]] == 0:
+                del count2[s2[i - len1]]
+
+            if count1 == count2:
+                return True
+
+        return False
+
+        
+
+
+        
