0e2d3ae3be4016124982f6117abd16c98d464570
siddu11-1 <givvasiddhartha@gmail.com>
2025-11-01 18:01:34 +0530

    Time: 64 ms (64.6%), Space: 21.3 MB (46.6%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,18 +1,4 @@
-'''class Solution(object):
-    def maxSubArray(self, nums):
-       maxsum=float('-inf')
-       for i in range(len(nums)):
 
-        
-            for j in range(i+1,len(nums)+1):
-                subarray=nums[i:j]
-                currentsum=sum(subarray)
-                if currentsum>maxsum:
-                    maxsum=currentsum
-       return maxsum
-nums = list(map(int, input("Enter space-separated integers: ").split()))
-sol = Solution()
-print(sol.maxSubArray(nums))'''
 class Solution(object):
     def maxSubArray(self, nums):
         max_current = max_global = nums[0]
@@ -21,14 +7,7 @@ class Solution(object):
             if max_current > max_global:
                 max_global = max_current
         return max_global
-        '''maxsum=float('-inf')
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)+1):
-                subarray=nums[i:j]
-                currentsum=sum(subarray)
-                if currentsum>maxsum:
-                    maxsum=currentsum
-        return maxsum'''
+     
                          
         
 
